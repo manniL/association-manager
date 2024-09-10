0bf3051404ef9e7c5a493db87b0c06f36faeb343
Alexander <github@lichter.io>
2024-09-10 19:11:30 +0200

    wip

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,6 +1,9 @@
 import { createBasicAuthMiddleware } from "h3-basic-auth";
 
 export default defineEventHandler(async (event) => {
+  if(event.path.startsWith("/api/_hub")) {
+    return
+  }
   const { sessionSecret, username, password } = useRuntimeConfig(event).auth;
   return createBasicAuthMiddleware({
     sessionSecret,
